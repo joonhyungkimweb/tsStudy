e0c5a81c895b34c58af7a92d1468e0dbd9d320e1
nick <nicolas.ai.nomis@gmail.com>
2022-03-15 16:49:28 +0900

    [feat] 23 리터럴 타입

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-function combine(n1, n2) {
-    if (typeof n1 === "number" && typeof n2 === "number") {
-        return n1 + n2;
+function combine(n1, n2, conversion) {
+    var result;
+    if ((typeof n1 === "number" && typeof n2 === "number") ||
+        conversion === "toNum") {
+        result = +n1 + +n2;
     }
     else {
-        return n1.toString() + n2.toString();
+        result = n1.toString() + n2.toString();
     }
+    return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, "toNum");
 console.log(combinedAges);
-var combineNames = combine("Kim", "Kelly");
+var combinedStringAges = combine("30", "26", "toNum");
+console.log(combinedStringAges);
+var combineNames = combine("Kim", "Kelly", "toString");
 console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,14 +1,28 @@
-function combine(n1: number | string, n2: number | string) {
-  if (typeof n1 === "number" && typeof n2 === "number") {
-    return n1 + n2;
+function combine(
+  n1: number | string,
+  n2: number | string,
+  conversion: "toNum" | "toString"
+) {
+  let result: number | string;
+  if (
+    (typeof n1 === "number" && typeof n2 === "number") ||
+    conversion === "toNum"
+  ) {
+    result = +n1 + +n2;
   } else {
-    return n1.toString() + n2.toString();
+    result = n1.toString() + n2.toString();
   }
+
+  return result;
 }
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "toNum");
 
 console.log(combinedAges);
 
-const combineNames = combine("Kim", "Kelly");
+const combinedStringAges = combine("30", "26", "toNum");
+
+console.log(combinedStringAges);
+
+const combineNames = combine("Kim", "Kelly", "toString");
 
 console.log(combineNames);
