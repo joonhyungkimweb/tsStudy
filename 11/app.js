988a804abe11a98454dbf2a1a9eca7f189616e46
nick <nicolas.ai.nomis@gmail.com>
2022-03-15 15:26:04 +0900

    [feat] 16 객체 형태

--- 11/app.js
+++ 11/app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, printResult, phrase) {
-    var result = n1 + n2;
-    if (printResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Resul is : ";
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: "Nick",
+    age: 30
+};
+console.log(person.name);
--- 11/app.ts
+++ 11/app.ts
@@ -1,15 +1,6 @@
-function add(n1: number, n2: number, printResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (printResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+const person = {
+  name: "Nick",
+  age: 30,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Resul is : ";
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
