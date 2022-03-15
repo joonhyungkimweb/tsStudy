8759144b9cd900f45ac4af599c1b5b693d6984ce
nick <nicolas.ai.nomis@gmail.com>
2022-03-15 15:31:11 +0900

    [feat] 18 배열 타입

--- 11/app.ts
+++ 11/app.ts
@@ -1,6 +1,15 @@
 const person = {
   name: "Nick",
   age: 30,
+  hobbies: ["Gaming", "Cooking"],
 };
 
+let favoriteActivities: string[];
+
+favoriteActivities = ["Gaming"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
