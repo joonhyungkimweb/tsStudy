91e48e6ea820b2fe62ae2110913afbdf4fd5eabc
nick <nicolas.ai.nomis@gmail.com>
2022-03-16 10:40:51 +0900

    [feat] 함수 타입 및 콜백 28

--- app.js
+++ app.js
@@ -4,4 +4,19 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result : ", num);
 }
+function addANdHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    var a = cb(result);
+    console.log(a);
+    return a;
+}
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(4, 4));
+addANdHandle(1, 2, function (num) {
+    console.log(num);
+    return num + 4;
+});
